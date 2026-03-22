8d916e703a82b727dc72f9946c13b2c7ae9b421e
Dominique Boeckli <dboeckli@gmail.com>
2026-03-22 09:24:17 +0100

    fix lombok 2

--- src/test/java/ch/bpm/workflow/example/common/ActuatorInfoTest.java
+++ src/test/java/ch/bpm/workflow/example/common/ActuatorInfoTest.java
@@ -12,7 +12,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.startsWith;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
