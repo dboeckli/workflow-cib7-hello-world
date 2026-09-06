f39f1cc1773827eaacde85fd0dd7f607d3713f95
Dominique Boeckli <dboeckli@gmail.com>
2026-09-06 12:48:33 +0200

    feat(.run): switch helm run configs to PowerShell and add test-k8s runner (#188)

--- .run/scripts/uninstall-k8s.ps1
+++ .run/scripts/uninstall-k8s.ps1
@@ -1,6 +1,9 @@
 cd target/helm/repo
 
-$file = Get-ChildItem -Filter workflow-cib7-hello-world-v*.tgz | Select-Object -First 1
+$file = Get-ChildItem -Filter workflow-cib7-hello-world-*.tgz | Select-Object -First 1
 $APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
 
 helm uninstall $APPLICATION_NAME --namespace workflow-cib7-hello-world
+
+kubectl delete pod -n workflow-cib7-hello-world --field-selector=status.phase==Succeeded
+kubectl delete pod -n workflow-cib7-hello-world --field-selector=status.phase==Failed
